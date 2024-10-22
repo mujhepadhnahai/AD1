b6a1cf8c25d079c6c66a925f2ad5212ac2ff3924
Guru Prasad <guruprasadpanda4646@gmail.com>
2024-10-22 12:31:07 +0530

    hjfh

--- assignment/asgn_4/q1.java
+++ assignment/asgn_4/q1.java
@@ -1,5 +1,30 @@
 //bubblesort
 package AD1.assignment.asgn_4;
 public class q1 {
-    
+    public static void main(String[] args) {
+        int arr [] ={6,4,8,9,3,1};
+        System.out.println("------Original Array------");
+        printArray(arr);
+        bubbleSort(arr);
+        System.out.println("------Sorted Array--------");
+        printArray(arr);
+    }
+
+    static void bubbleSort(int[] arr){
+        for(int i = 0 ; i<arr.length;i++){
+           int j = i;
+           while (j>0 && arr[j-1]>arr[j]) {
+            int temp = arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j] = temp;
+            j--;
+           }
+        }
+    }
+
+    static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + ", ");
+        }System.out.println();
+    }
 }
--- assignment/asgn_4/q3.java
+++ assignment/asgn_4/q3.java
@@ -1,5 +1,23 @@
 //section sort
 package AD1.assignment.asgn_4;
 public class q3 {
-    
+    public static void main(String[] args) {
+        int arr [] ={6,4,8,9,3,1};
+        System.out.println("------Original Array------");
+        printArray(arr);
+        selectionSort(arr);
+        System.out.println("------Sorted Array------");
+        printArray(arr);
+    }
+
+    static void selectionSort(int[] arr){
+      
+    }
+
+    static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + ", ");
+        }
+    }
 }
